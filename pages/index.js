5209510d5c36a0dfdc8f08de95f9242e1cd5a22e
leonardoTavaesM <leomaltadesa0712@gmail.com>
2024-07-06 00:38:21 -0300

    updt pullrequest preview

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HomeScreen() {
   return (
     <div>
-      <h1>Home pageee</h1>
+      <h1>Home page [alteração]</h1>
       <Link href="/sobre">ir para sobre</Link>
     </div>
   );
--- scripts/pullRequestPreview.mjs
+++ scripts/pullRequestPreview.mjs
@@ -3,8 +3,11 @@ import { execSync } from "child_process";
 console.log("[DEPLOY_PREVIEW]: START");
 const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: "utf8" }).toString();
-console.log("[DEPLOY_PREVIEW]: END");
 console.log(output);
+const outputLines = output.split("\n");
+const DEPLOY_URL = outputLines[outputLines.length - 1];
+console.log("[DEPLOY_PREVIEW]: END");
+console.log("You can see the deploy preview here:", DEPLOY_URL);
 
 // console.log("[GITHUB_COMMENT]: START");
 // console.log("[GITHUB_COMMENT]: END");
