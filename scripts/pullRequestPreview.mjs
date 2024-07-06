febbcf7e833d653ee5eca712e2a985e52d44819f
leonardoTavaesM <leomaltadesa0712@gmail.com>
2024-07-06 16:39:48 -0300

    feat: adding fetch to github comment

--- scripts/pullRequestPreview.mjs
+++ scripts/pullRequestPreview.mjs
@@ -9,5 +9,28 @@ const DEPLOY_URL = outputLines[outputLines.length - 1];
 console.log("[DEPLOY_PREVIEW]: END");
 console.log("You can see the deploy preview here:", DEPLOY_URL);
 
-// console.log("[GITHUB_COMMENT]: START");
-// console.log("[GITHUB_COMMENT]: END");
+console.log("[GITHUB_COMMENT]: START");
+
+fetch(
+  `https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${GITHUB_PR_NUMBER}/comments`,
+  {
+    headers: defaultHeaders,
+    method: "POST",
+    body: JSON.stringify({
+      body: GH_COMMENT,
+    }),
+  }
+)
+  .then((response) => {
+    if (response.ok) return response.json();
+    throw new Error(response.statusText);
+  })
+  .catch((err) => {
+    console.log("[COMMENT_ON_GITHUB: ERROR]");
+    console.error(err);
+  })
+  .finally(() => {
+    console.log("[COMMENT_ON_GITHUB: END]");
+  });
+
+console.log("[GITHUB_COMMENT]: END");
